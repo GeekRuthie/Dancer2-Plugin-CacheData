b6fc713ef52875fedbfdb53ff931c99e2253f19c
D Ruth Holloway <ruth@hiruthie.me>
2024-07-02 09:49:28 -0400

    Initial structural commit

---
+++ lib/Dancer2/Plugin/CacheData.pm
@@ -0,0 +1,37 @@
+package Dancer2::Plugin::CacheData;
+use strict;
+use warnings;
+
+# VERSION
+# AUTHORITY
+# ABSTRACT: 
+
+use Dancer2::Plugin;
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 KEYWORDS
+
+=head1 CONFIGURATION
+
+=head1 ACKNOWLEDGEMENTS
+
+Special thanks to my employer, Clearbuilt, for giving me time to work on this module.
+
+=head1 SEE ALSO
+ 
+=over 4
+
+=item * L<Dancer2>
+ 
+=back
+
+=cut
